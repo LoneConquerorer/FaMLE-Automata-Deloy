a87a61c2577c0d23a7fc9141823a14b72a8439ea
Jerry Zhang <jzhang56@mit.edu>
2020-05-25 17:40:43 -0500

    init-commit

---
+++ app.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+const { addUser, removeUser, getUser } = require("./users.js");
+
+const {
+  addPreviewID,
+  removePreviewID,
+  addPreviewLoc,
+  pushPreview,
+  currentBoard,
+  updateCells,
+  addNoteLoc,
+  clearCells,
+  clearNotes
+} = require("./game.js");
+
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+app.use(cors());
+
+var tempo = 240;
+var time = 0;
+
+const checkTempo = () => {
+  return tempo;
+};
+
+const updateStep = () => {
+  console.log(time++);
+  const { remove, cells } = updateCells();
+  io.emit("cellsUpdate", { remove, cells });
+  setTimeout(updateStep, (60 / checkTempo()) * 1000);
+};
+
+io.on("connection", socket => {
+  console.log("player connected");
+  // player side sockets
+  socket.on("join", ({ name }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name });
+    if (error) return callback(error);
+    addPreviewID(socket.id);
+
+    // Tells user they have joined
+    socket.emit("message", {
+      user: "admin",
+      text: "You have joined the chat"
+    });
+
+    // Tells all other users that new user has joined
+    socket.broadcast.emit("message", {
+      user: "admin",
+      text: `${user.name} has joined`
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const thisUser = getUser(socket.id);
+    io.emit("message", { user: thisUser.name, text: message });
+    callback();
+  });
+
+  // Game related sockets
+
+  socket.on("boardInit", callback => {
+    console.log("initializing board");
+    callback(currentBoard());
+  });
+
+  socket.on("subscribeToTimer", callback => {
+    callback(tempo);
+    socket.emit("tempo", tempo);
+  });
+
+  socket.on("tempoChange", t => {
+    console.log("client changed tempo to ", t);
+    tempo = Math.max(30, parseInt(t));
+    tempo = Math.min(tempo, 720);
+    time = 0;
+    io.emit("tempo", tempo);
+  });
+
+  socket.on("previewClick", ({ loc, value }) => {
+    console.log("preview " + loc);
+    addPreviewLoc(socket.id, loc, value);
+    socket.broadcast.emit("previewUpdate", { loc, value });
+  });
+
+  // pushes the current preview
+  socket.on("previewPush", () => {
+    console.log("pushed!");
+    pushPreview(socket.id);
+  });
+
+  socket.on("updateNotes", ({ loc, type, value }) => {
+    addNoteLoc(loc, type, value);
+    socket.broadcast.emit("notesUpdate", { loc, type, value });
+  });
+
+  socket.on("clearNotes", () => {
+    clearNotes();
+    io.emit("notesCleared");
+  });
+
+  socket.on("clearCells", () => {
+    clearCells();
+    io.emit("cellsCleared");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("player disconnected");
+    const user = removeUser(socket.id);
+    const { arr } = removePreviewID(socket.id);
+    if (user) {
+      io.emit("message", { user: "admin", text: `${user.name} has left` });
+    }
+
+    // remove the preview of array
+    io.emit("previewClose", { arr });
+  });
+});
+
+updateStep();
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ build/precache-manifest.2fc45f78697ac4bd65a2c2b5c0933bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f280f5140ec5247c71de04577153bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "542da4260d3f3ad0399c",
+    "url": "/static/css/main.14418411.chunk.css"
+  },
+  {
+    "revision": "d9ecc669384e47b9255b",
+    "url": "/static/js/2.937c9f1c.chunk.js"
+  },
+  {
+    "revision": "1988d8964f7d42e14339485472d076ee",
+    "url": "/static/js/2.937c9f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542da4260d3f3ad0399c",
+    "url": "/static/js/main.f1133563.chunk.js"
+  },
+  {
+    "revision": "76c9905694a485adaa7a",
+    "url": "/static/js/runtime-main.fa965e14.js"
+  }
+]);
