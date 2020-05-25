578682a8b65ecf66860b6ea9ebc8cfb76c06f23c
Jerry Zhang <jzhang56@mit.edu>
2020-05-25 17:43:36 -0500

    add deployable

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("server is up and running");
+router.use(express.static(path.join(__dirname, "build")));
+
+router.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 module.exports = router;
