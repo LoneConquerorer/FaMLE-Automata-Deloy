7ac160f1773eb66d86ec259528f0d05889b66228
Jerry Zhang <jzhang56@mit.edu>
2020-05-25 17:58:53 -0500

    deploy3

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.use(cors());
