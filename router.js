750b276e432774497a91daa7be1b859227d10b6e
Jerry Zhang <jzhang56@mit.edu>
2020-05-25 18:09:55 -0500

    deploy test 5

--- router.js
+++ router.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.use(express.static(path.join(__dirname, "build")));
-
-router.get("/*", (req, res) => {
+router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
