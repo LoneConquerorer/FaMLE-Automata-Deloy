3f7d8fda1f321e1e226b9af274e028b01859244a
Jerry Zhang <jzhang56@mit.edu>
2020-05-25 21:03:03 -0500

    remove

--- router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const root = require("path").join(__dirname, "build");
-
-router.get("*", (req, res) => {
-  res.sendFile("index.html", { root });
-});
-
-module.exports = router;
