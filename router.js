fd873b7deb29da482bb400e6275cebefb3779846
Jerry Zhang <jzhang56@mit.edu>
2020-05-25 20:53:35 -0500

    typo

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const root = require("path").join(__dirname, "client", "build");
+const root = require("path").join(__dirname, "build");
 
 router.get("*", (req, res) => {
   res.sendFile("index.html", { root });
